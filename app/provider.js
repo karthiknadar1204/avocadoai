bc86cf7af9e6bed74c3fe9eb8f518fe154e2c10b
karthik <karthiknadar1204@gmail.com>
2024-09-28 10:39:49 +0530

    setup provider and update db

--- app/provider.js
+++ app/provider.js
@@ -1,9 +1,35 @@
-import React from 'react'
+"use client";
 
-const Provider = ({children}) => {
-  return (
-    <div>provider</div>
-  )
+import { db } from "@/configs/db";
+import { Users } from "@/configs/schema";
+import { useUser } from "@clerk/clerk-react";
+import { eq } from "drizzle-orm";
+import React, { useEffect } from "react";
+
+function Provider({ children }) {
+  const { user } = useUser();
+
+  useEffect(()=>{
+    user&&isNewUser();
+  },[user]);
+
+  const isNewUser = async () => {
+    const result = await db
+      .select()
+      .from(Users)
+      .where(eq(Users.email, user?.primaryEmailAddress?.emailAddress));
+
+    console.log("result", result);
+
+    if (!result[0]) {
+      await db.insert(Users).values({
+        name: user?.fullName,
+        email: user?.primaryEmailAddress?.emailAddress,
+        image: user?.imageUrl,
+      });
+    }
+  };
+  return <div>{children}</div>;
 }
 
-export default Provider
+export default Provider;
